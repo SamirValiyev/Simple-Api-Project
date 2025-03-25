177079558d8dfc1055acb0c792e0268d8fe85239
SamirValiyev <vliyevsamir5@gmail.com>
2025-03-25 17:02:05 +0400

    Context Added

---
+++ Persistence/Persistence/Context/SimpleDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Context
+{
+    public class SimpleDbContext:DbContext
+    {
+        public SimpleDbContext(DbContextOptions<SimpleDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
