6ae39fb3ee0f431d853e5f06f5cc2b6e4ac105b8
SamirValiyev <vliyevsamir5@gmail.com>
2025-03-25 15:26:48 +0400

    Add project files.

---
+++ Core/Domain/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public  string? Definition { get; set; }
+        public List<User>? Users { get; set; }
+    }
+}
