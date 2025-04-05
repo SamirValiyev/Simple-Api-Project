cede001a747cb4d4cd534a22e5492280e2784760
SamirValiyev <vliyevsamir5@gmail.com>
2025-04-05 19:47:51 +0400

    RemoveProduct  Completed

--- SimpleApiProjectUI/Controllers/ProductController.cs
+++ SimpleApiProjectUI/Controllers/ProductController.cs
@@ -21,7 +21,7 @@ namespace SimpleApiProjectUI.Controllers
         public async Task<IActionResult> GetProducts()
         {
             var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
-            if (token != null)
+            if (token is not null)
             {
                 var client = _httpClientFactory.CreateClient();
                 client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(JwtBearerDefaults.AuthenticationScheme, token);
@@ -44,7 +44,7 @@ namespace SimpleApiProjectUI.Controllers
         public async Task<IActionResult> Update(int id)
         {
             var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
-            if (token != null)
+            if (token is not null)
             {
                 var client = _httpClientFactory.CreateClient();
                 client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(JwtBearerDefaults.AuthenticationScheme, token);
@@ -68,21 +68,59 @@ namespace SimpleApiProjectUI.Controllers
             if (ModelState.IsValid)
             {
                 var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
-                if (token != null)
+                if (token is not null)
                 {
                     var client = _httpClientFactory.CreateClient();
                     client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(JwtBearerDefaults.AuthenticationScheme, token);
-
                     var jsonData = JsonSerializer.Serialize(model);
                     var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-                    var response = await client.PutAsync("http://localhost:5128/api/products",content);
+                    var response = await client.PutAsync("http://localhost:5128/api/products", content);
                     if (response.IsSuccessStatusCode)
-                        return RedirectToAction("GetProducts","Product");
+                        return RedirectToAction("GetProducts", "Product");
                     else
-                        ModelState.AddModelError(String.Empty, "Error occurred while updating product");  
+                        ModelState.AddModelError(String.Empty, "Error occurred while updating product");
                 }
             }
             return View(model);
+        }
+
+        public async Task<IActionResult> Remove(int id)
+        {
+            var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
+            if (token is not null)
+            {
+                var client = _httpClientFactory.CreateClient();
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(JwtBearerDefaults.AuthenticationScheme, token);
+                var response = await client.DeleteAsync($"http://localhost:5128/api/products/{id}");
+            }
+            return RedirectToAction("GetProducts");
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var model = new ProductCreateModel();
+            var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
+            if (token is not null)
+            {
+                var client = _httpClientFactory.CreateClient();
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(JwtBearerDefaults.AuthenticationScheme, token);
+
+                var response = await client.GetAsync("http://localhost:5128/api/categories");
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonData=await response.Content.ReadAsStringAsync();    
+                    var data=JsonSerializer.Deserialize<List<CategoryResponseModel>>(jsonData,new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy=JsonNamingPolicy.CamelCase
+                    });
+                    model.Categories = new Microsoft.AspNetCore.Mvc.Rendering.SelectList(data, "Id", "Definition");
+                    return View(model);
+                }
+            }
+            return RedirectToAction("GetProducts");
         }
     }
 }
